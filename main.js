fb50aa5122510c6259e8451155a68f6d9530c47a
NachoCheeseOfDoom <85528067+NachoCheeseOfDoom@users.noreply.github.com>
2024-06-13 15:24:53 -0600

    Finished the Blog Space project, refactored some code and created functions

--- main.js
+++ main.js
@@ -1,30 +1,57 @@
 const blogContainer = document.getElementById("blog-list");
-const newPost = document.getElementById("new-post");
-newPost.addEventListener("submit", (e) => {
+const postForm = document.getElementById("new-post");
+
+fetch("https://apis.scrimba.com/jsonplaceholder/posts")
+  .then((res) => res.json())
+  .then((data) => {
+    const postArr = data.splice(0, 2);
+
+    postArr.forEach((post) => {
+      renderPosts(post);
+    });
+
+  });
+
+postForm.addEventListener("submit", (e) => {
   e.preventDefault();
-  const form = new FormData(newPost);
+  const form = new FormData(postForm);
   const postTitle = form.get("title");
   const postBody = form.get("body");
+
+  postForm.reset();
+
+  e.target.title.focus()
 
   const postData = {
     title: postTitle,
     body: postBody,
   };
 
-  console.log(postData);
+  sendData(postData)
 });
 
-fetch("https://apis.scrimba.com/jsonplaceholder/posts", { method: "GET" })
-  .then((res) => res.json())
-  .then((data) => {
-    const postArr = data.splice(0, 5);
-    html = "";
-    postArr.forEach((post) => {
-      html += `
+function sendData(data) {
+  const options = {
+    method: 'POST',
+    body: JSON.stringify(data),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  }
+
+  fetch("https://apis.scrimba.com/jsonplaceholder/posts", options)
+    .then(res => res.json())
+    .then(data => {
+      renderPosts(data);
+    })
+}
+
+function renderPosts(post) {
+  const html = `
       <h3 style='text-transform: capitalize;'>${post.title}</h3>
       <p>${post.body}</p>
       <hr/>
-      `;
-    });
-    blogContainer.innerHTML = html;
-  });
+      ${blogContainer.innerHTML}
+  `
+  blogContainer.innerHTML = html;
+}
